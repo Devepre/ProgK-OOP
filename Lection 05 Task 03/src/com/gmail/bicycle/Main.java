b5599a01fbb62f9e35149ce1976dde6f2578b79a
Devepre <bicycle2life@gmail.com>
2017-11-01 18:21:42 +0200

    changed to use any possible method to save\load info

--- Lection 05 Task 03/src/com/gmail/bicycle/Main.java
+++ Lection 05 Task 03/src/com/gmail/bicycle/Main.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 import java.util.Date;
 
 import com.gmail.bicycle.dao.factories.DAOFactory;
+import com.gmail.bicycle.dao.factories.JSONDAOFactory;
+import com.gmail.bicycle.dao.factories.PlainTextDAOFactory;
 import com.gmail.bicycle.dao.interfaces.GroupDAO;
-import com.gmail.bicycle.json.JsonRealization;
 import com.gmail.bicycle.json.JsonParser;
 import com.gmail.bicycle.model.Group;
 import com.gmail.bicycle.model.Student;
@@ -13,7 +14,7 @@ import com.gmail.bicycle.model.Student;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("Creating some default group...");
 
 		Group group = new Group("Chemistry17");
 		Student st1 = new Student("Jack", "Brown", new Date(0, 1, 23, 23, 1), true, 23.9);
@@ -21,49 +22,69 @@ public class Main {
 		Student st3 = new Student("Bill", "Yellow", new Date(), true, 23);
 		Student st4 = new Student("Anna", "Smith", new Date(), false, 213.34);
 		try {
-			//group.addStudent(st1);
+			group.addStudent(st1);
 			group.addStudent(st2);
 			group.addStudent(st3);
 			group.addStudent(st4);
 
-			System.out.println("Created group is:");
+			System.out.println("Group created:");
 			System.out.println(group);
-			System.out.println();
 		} catch (GroupOutOfBoundsException e) {
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
 
-/*		DAOFactory plainTextFactory = DAOFactory.getDAOFactory(DAOFactory.PLAIN_TEXT);
+		System.out.println("<<<Demo using DAO>>>");
+		System.out.println("<<<File name used from PlainTextDAOFactory and JSONDAOFactory configuration>>>");
+		System.out.println();
+
+		DAOFactory plainTextFactory = DAOFactory.getDAOFactory(DAOFactory.PLAIN_TEXT);
 		GroupDAO groupDAO = plainTextFactory.getGroupDAO();
 		try {
-			System.out.println("Saving group usint Plain Text method");
+			System.out.println("Saving group using Plain Text DAO method. Mode: adding to file if exist");
+			System.out.println("File name is: " + PlainTextDAOFactory.FILE_NAME);
 			groupDAO.saveGroup(group);
-			System.out.println("Loading group using Plain Text method");
+			System.out.println("Loading group using Plain Text DAO method");
 			Group loadedGroup = groupDAO.loadGroup();
 			System.out.println(loadedGroup);
 
 		} catch (IOException e) {
 			e.printStackTrace();
-		}*/
-		
+		}
+
+		DAOFactory jsonFactory = DAOFactory.getDAOFactory(DAOFactory.JSON);
+		GroupDAO jsonGrpupDAO = jsonFactory.getGroupDAO();
+
+		try {
+			System.out.println("Saving group using JSON method");
+			System.out.println("File name is: " + JSONDAOFactory.FILE_NAME);
+			jsonGrpupDAO.saveGroup(group);
+			System.out.println("Loading group using JSON method");
+			Group loadededJsonGroup = jsonGrpupDAO.loadGroup();
+			System.out.println(loadededJsonGroup);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		Group loadedGroup = new Group();
-		String fileName = "data.json";										
+		String fileName = "data.json";
+		System.out.printf("<<<Demo using JSON parser directly. Using '%s' file>>>", fileName);
+		System.out.println();
 		try {
 			JsonParser jsonParser = JsonParser.getParser();
+			System.out.println("Saving group");
 			jsonParser.serialize(group, fileName);
-			loadedGroup = (Group) jsonParser.deserialize(Group.class, fileName);			
+			loadedGroup = (Group) jsonParser.deserialize(Group.class, fileName);
 			System.out.println("Loaded group is:");
 			System.out.println(loadedGroup);
-			System.out.println();
 		} catch (SecurityException | IOException | ReflectiveOperationException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-						
-		System.out.println("End of program");
+
+		System.out.println("<<<End of program>>>");
 
 	}
 
