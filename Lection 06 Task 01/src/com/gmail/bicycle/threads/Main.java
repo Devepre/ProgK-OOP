17b9d072f156a2aa47cbd8e8e6eef08837552e0d
Devepre <bicycle2life@gmail.com>
2017-11-02 21:47:04 +0200

    say Stop! 2SemenSemenych

--- Lection 06 Task 01/src/com/gmail/bicycle/threads/Main.java
+++ Lection 06 Task 01/src/com/gmail/bicycle/threads/Main.java
@@ -1,32 +1,57 @@
 package com.gmail.bicycle.threads;
 
-import java.math.BigInteger;
-
 public class Main {
+	private static Thread[] threads;
+	private static FactorialThread[] factorialThreadArray;
 
 	public static void main(String[] args) {
-		System.out.println("Main start");
-		
-		//BigInteger bigInt = Factorial.calculateRecursively(20000); //stack overflow		
-		//System.out.println(bigInt);
-		
-		int number = 100;
-		
-		FactorialThread factorialThread = new FactorialThread(number);
-		System.out.println("Main: result is " + factorialThread.getResult());
-		Thread thread = new Thread(factorialThread);		
-		thread.start();
-		
-//		try {
-//			thread.join();
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-		
-		System.out.println("Main: result is " + factorialThread.getResult());
-		
-		System.out.println("End of main");
+		System.out.println("main method start");
 
+		int n = 100;
+		generateEverything(n);
+		startThreads(threads);
+		joinThreads(threads);
+		showResults();
+
+		System.out.println("End of main method");
+	}
+
+	private static void generateEverything(int n) {
+		factorialThreadArray = new FactorialThread[n];
+		threads = generateThreads(n);
+	}
+
+	private static Thread[] generateThreads(int n) {
+		Thread[] threads = new Thread[n];
+		for (int i = 0; i < n; i++) {
+			FactorialThread factorialThread = new FactorialThread(i + 1);
+			factorialThreadArray[i] = factorialThread;
+			Thread thread = new Thread(factorialThread);
+			threads[i] = thread;
+		}
+		return threads;
+	}
+
+	private static void startThreads(Thread... threads) {
+		for (Thread thread : threads) {
+			thread.start();
+		}
+	}
+
+	private static void joinThreads(Thread... threads) {
+		for (int i = 0; i < threads.length; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void showResults() {
+		for (FactorialThread factorialThread : factorialThreadArray) {
+			System.out.println(factorialThread.getNumber() + "!=" + factorialThread.getResult() + " calculated by" + factorialThread.getName());
+		}
 	}
 
 }
