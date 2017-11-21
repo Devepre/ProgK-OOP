56b34223c8b278c86e650c131fbbb2240203af2a
Devepre <bicycle2life@gmail.com>
2017-11-21 17:58:55 +0200

    Solving lesson 9, task additional 2,3,4

---
+++ Lection 09a Task 0234/src/com/gmail/bicycle/func/Main.java
@@ -0,0 +1,79 @@
+package com.gmail.bicycle.func;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] array = new int[] { 2, -2, 4, 435, -12 };
+		System.out.println(findDigit(array));
+		String data = "AB ab";
+
+		int[] arr = generateArray(data);
+		for (int i : arr) {
+			System.out.println(i);
+		}
+
+		System.out.println("Max is:");
+		int max = findMix("in.txt");
+		System.out.println(max);
+	}
+
+	public static int findDigit(int[] array) {
+		int result = Arrays.stream(array)
+				.map(n -> Math.abs(n))
+				.min().orElseThrow(NoSuchElementException::new);
+
+		return result;
+	}
+
+	public static int[] generateArray(String data) {
+		int[] result = data.chars()
+				.map(n -> (int) n)
+				.toArray();
+
+		return result;
+	}
+	
+	public static int findMix(String fileName) {
+		int result = -0;
+		try {
+			result = Files.lines(Paths.get(fileName))
+					.map(n -> Integer.parseInt(n))
+					.max(Integer::compare).orElseThrow(NoSuchElementException::new);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
