83c39054e2c0c76d9bcb8a57e859d8d8a005dd9d
Devepre <bicycle2life@gmail.com>
2017-11-24 22:49:06 +0200

    adding more flexibility

--- Lection 10 Task 01/src/com/gmail/bicycle/translation/TranslationDictionary.java
+++ Lection 10 Task 01/src/com/gmail/bicycle/translation/TranslationDictionary.java
@@ -4,6 +4,6 @@ import java.util.AbstractMap;
 
 public interface TranslationDictionary {
 	public void addTranslation(String input, String output);
-	public AbstractMap<String, String> getDictionary();
+	public AbstractMap<? extends String, ? extends String> getDictionary();
 
 }
