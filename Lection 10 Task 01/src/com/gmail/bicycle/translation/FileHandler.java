61c5da5cdf2b9e4a98eff831f67b7c50f8438280
Devepre <bicycle2life@gmail.com>
2017-11-24 23:40:42 +0200

    removing unnecessary methods

--- Lection 10 Task 01/src/com/gmail/bicycle/translation/FileHandler.java
+++ Lection 10 Task 01/src/com/gmail/bicycle/translation/FileHandler.java
@@ -30,35 +30,6 @@ public class FileHandler {
 		return result;
 	}
 
-	public static StringBuilder getEqualsToFile(StringBuilder data, File file, String delimiter)
-			throws IOException, IllegalArgumentException {
-		checkIfDir(file);
-
-		StringBuilder equals = new StringBuilder(data.length());
-		try (BufferedReader bReader = new BufferedReader(new FileReader(file))) {
-			String str;
-			while ((str = bReader.readLine()) != null) {
-				String[] words = str.split("\\W+");
-				for (String string : words) {
-					if (!string.equals("") && data.indexOf(string) > -1) {
-						equals.append(string).append(delimiter);
-					}
-				}
-			}
-		}
-
-		return equals;
-	}
-
-	public static void saveTextToFile(StringBuilder data, File file) throws IOException, IllegalArgumentException {
-		checkIfDir(file);
-
-		try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(file))) {
-			bWriter.write(data.toString());
-		}
-
-	}
-
 	protected static void checkIfDir(File file) throws IllegalArgumentException {
 		if (file.isDirectory()) {
 			throw new IllegalArgumentException(file.getAbsolutePath() + " (argument can't be directory)");
